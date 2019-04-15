de352cd5c9da27e91d172afbfdec084f91d16f33
Abu Sufyan <44092342+abusufyanqazi@users.noreply.github.com>
2019-04-15 18:15:06 +0500

    u

--- CISDBAPI/Controllers/CreditAdjustmentController.cs
+++ CISDBAPI/Controllers/CreditAdjustmentController.cs
@@ -10,12 +10,9 @@ namespace DashBoardAPI.Controllers
 {
     public class CreditAdjustmentController : ApiController
     {
-        //string token, string code = "15",string age="1", string phase="1", string trf="01"
-        //string token, string code = "15", string batchFrom="01", string batchTo="02", string unitFlag="0"
-        public CreditAdjustments Get(string code = "15",string age="01", string phase="20", string trf="0")
+        public CreditAdjustments Get(string code = "15", string batch = "01", string units = "0")
         {
-            return new DBoardBridge().GetCRAdjustments(code, age, phase,
-                char.Parse(trf.Substring(0, 1)));
+            return new DBoardBridge().GetCRAdjustments(code, batch, char.Parse(units.Substring(0, 1)));
         }
     }
 }
